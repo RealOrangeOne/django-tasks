a48cc0b6d290be2c0fbd9cf8bb08b0578f729d45
Jure Cuhalev <jure@cuhalev.com>
2024-06-08 12:56:09 +0200

    Add types

--- django_tasks/backends/database/admin.py
+++ django_tasks/backends/database/admin.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from django.contrib import admin
+from django.http import HttpRequest
 
 from .models import DBTaskResult
 
@@ -8,8 +11,12 @@ class DBTaskResultAdmin(admin.ModelAdmin):
     list_display = ("id", "task_path", "status", "priority", "queue_name")
     list_filter = ("status", "priority", "queue_name")
 
-    def has_add_permission(self, request, obj=None):
+    def has_add_permission(
+        self, request: HttpRequest, obj: Optional[DBTaskResult] = None
+    ) -> bool:
         return False
 
-    def get_readonly_fields(self, request, obj=None):
+    def get_readonly_fields(
+        self, request: HttpRequest, obj: Optional[DBTaskResult] = None
+    ) -> list[str]:
         return [f.name for f in self.model._meta.fields]
