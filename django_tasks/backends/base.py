136fc056838c6df9e2b31ca8dd74ccc38c94f861
Jure Cuhalev <jure@cuhalev.com>
2024-06-08 15:18:32 +0200

    Py 3.8 types

--- django_tasks/backends/base.py
+++ django_tasks/backends/base.py
@@ -1,6 +1,6 @@
 from abc import ABCMeta, abstractmethod
 from inspect import iscoroutinefunction
-from typing import Any, TypeVar
+from typing import Any, TypeVar, List
 
 from asgiref.sync import sync_to_async
 from django.core.checks.messages import CheckMessage
@@ -94,7 +94,7 @@ class BaseTaskBackend(metaclass=ABCMeta):
         # HACK: `close` isn't abstract, but should do nothing by default
         return None
 
-    def check(self, **kwargs: Any) -> list[CheckMessage]:
+    def check(self, **kwargs: Any) -> List[CheckMessage]:
         raise NotImplementedError(
             "subclasses may provide a check() method to verify that task "
             "backend is configured correctly."
