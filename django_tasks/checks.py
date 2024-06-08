8f7f6e651f56eadf2232af5fb8c8600f1cbc0d50
Jure Cuhalev <jure@cuhalev.com>
2024-06-08 15:24:41 +0200

    Linter

--- django_tasks/checks.py
+++ django_tasks/checks.py
@@ -1,5 +1,4 @@
-from collections.abc import Sequence
-from typing import Any, List
+from typing import Any, List, Sequence
 
 from django.apps.config import AppConfig
 from django.core.checks.messages import CheckMessage
