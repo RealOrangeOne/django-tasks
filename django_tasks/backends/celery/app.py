eab648270ec952de09d9fdbed56e974c7f847e03
Matias Bordese <mbordese@gmail.com>
2024-07-08 17:12:36 -0300

    Lint issues, return task result

--- django_tasks/backends/celery/app.py
+++ django_tasks/backends/celery/app.py
@@ -5,17 +5,17 @@ from celery import Celery
 from django_tasks.task import DEFAULT_QUEUE_NAME
 
 # Set the default Django settings module for the 'celery' program.
-django_settings = os.environ.get('DJANGO_SETTINGS_MODULE')
+django_settings = os.environ.get("DJANGO_SETTINGS_MODULE")
 if django_settings is None:
-    raise ValueError('DJANGO_SETTINGS_MODULE environment variable is not set')
+    raise ValueError("DJANGO_SETTINGS_MODULE environment variable is not set")
 
-app = Celery('django_tasks')
+app = Celery("django_tasks")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.conf.task_default_queue = DEFAULT_QUEUE_NAME
 
--- django_tasks/backends/celery/backend.py
+++ django_tasks/backends/celery/backend.py
@@ -1,15 +1,20 @@
+from dataclasses import dataclass
 from typing import TypeVar
 
 from celery import shared_task
 from celery.app import default_app
 from celery.local import Proxy as CeleryTaskProxy
+from django.utils import timezone
 from typing_extensions import ParamSpec
 
 from django_tasks.backends.base import BaseTaskBackend
-from django_tasks.task import Task, TaskResult
+from django_tasks.task import ResultStatus, TaskResult
+from django_tasks.task import Task as BaseTask
+from django_tasks.utils import json_normalize
 
 if not default_app:
     from django_tasks.backends.celery.app import app as celery_app
+
     celery_app.set_default()
 
 
@@ -17,32 +22,49 @@ T = TypeVar("T")
 P = ParamSpec("P")
 
 
-class CeleryTask(Task):
-
-    celery_task: CeleryTaskProxy
+@dataclass
+class Task(BaseTask[P, T]):
+    celery_task: CeleryTaskProxy = None
     """Celery proxy to the task in the current celery app task registry."""
 
     def __post_init__(self) -> None:
-        # TODO: allow passing extra celery specific parameters?
         celery_task = shared_task()(self.func)
         self.celery_task = celery_task
         return super().__post_init__()
 
 
 class CeleryBackend(BaseTaskBackend):
-    task_class = CeleryTask
+    task_class = Task
     supports_defer = True
 
     def enqueue(
-        self, task: Task[P, T], args: P.args, kwargs: P.kwargs
+        self,
+        task: Task[P, T],  # type: ignore[override]
+        args: P.args,
+        kwargs: P.kwargs,
     ) -> TaskResult[T]:
         self.validate_task(task)
 
-        apply_async_kwargs = {
+        apply_async_kwargs: P.kwargs = {
             "eta": task.run_after,
         }
         if task.queue_name:
             apply_async_kwargs["queue"] = task.queue_name
         if task.priority:
             apply_async_kwargs["priority"] = task.priority
-        task.celery_task.apply_async(args, kwargs=kwargs, **apply_async_kwargs)
+
+        # TODO: a Celery result backend is required to get additional information
+        async_result = task.celery_task.apply_async(
+            args, kwargs=kwargs, **apply_async_kwargs
+        )
+        task_result = TaskResult[T](
+            task=task,
+            id=async_result.id,
+            status=ResultStatus.NEW,
+            enqueued_at=timezone.now(),
+            finished_at=None,
+            args=json_normalize(args),
+            kwargs=json_normalize(kwargs),
+            backend=self.alias,
+        )
+        return task_result
