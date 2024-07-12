c0be13c19808ec5e578ff6a0698184adc6c8bc0d
Jake Howard <git@theorangeone.net>
2024-07-12 17:04:46 +0100

    Use existing util to get connection

--- django_tasks/backends/database/utils.py
+++ django_tasks/backends/database/utils.py
@@ -1,19 +1,21 @@
 from contextlib import contextmanager
-from typing import Any, Generator, Union
+from typing import Any, Generator, Optional, Union
 from uuid import UUID
 
-from django.db import DEFAULT_DB_ALIAS, connections, transaction
+from django.db import transaction
 
 
 @contextmanager
-def exclusive_transaction(using: str = DEFAULT_DB_ALIAS) -> Generator[Any, Any, Any]:
+def exclusive_transaction(using: Optional[str] = None) -> Generator[Any, Any, Any]:
     """
     Wrapper around `transaction.atomic` which ensures transactions on SQLite are exclusive.
 
     This functionality is built-in to Django 5.1+.
     """
-    if connections[using].vendor == "sqlite":
-        with connections[using].cursor() as c:
+    connection = transaction.get_connection(using)
+
+    if connection.vendor == "sqlite":
+        with connection.cursor() as c:
             c.execute("BEGIN EXCLUSIVE")
             try:
                 yield
