80f22bcde9a77303d016bef3211f966ed446cd31
Jake Howard <git@theorangeone.net>
2024-11-21 13:53:53 +0000

    Add test for having no task backends defined

--- tests/tests/test_tasks.py
+++ tests/tests/test_tasks.py
@@ -245,3 +245,8 @@ class TaskTestCase(SimpleTestCase):
             import_string(test_tasks.noop_task_async.module_path),
             test_tasks.noop_task_async,
         )
+
+    @override_settings(TASKS={})
+    def test_no_backends(self) -> None:
+        with self.assertRaises(InvalidTaskBackendError):
+            test_tasks.noop_task.enqueue()
