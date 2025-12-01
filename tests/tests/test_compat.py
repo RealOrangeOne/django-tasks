6dfe6f2e7a93d0b8e8226361631a73bce0f5c627
Jake Howard <git@theorangeone.net>
2025-12-01 12:23:49 +0000

    Improve compatibility tests with Django 6.0

--- tests/tests/test_compat.py
+++ tests/tests/test_compat.py
@@ -3,11 +3,15 @@ from unittest import skipUnless
 from django import VERSION
 from django.test import SimpleTestCase, override_settings
 
-from django_tasks import compat, task_backends
+from django_tasks import compat, task, task_backends
 from django_tasks.backends.immediate import ImmediateBackend
-from django_tasks.base import Task
+from django_tasks.base import Task, TaskResult
 
 HAS_DJANGO_TASKS = VERSION >= (6, 0)
+
+
+def _test_task_func() -> None:
+    pass
 
 
 class DjangoCompatTestCase(SimpleTestCase):
@@ -17,7 +21,7 @@ class DjangoCompatTestCase(SimpleTestCase):
 
     @skipUnless(HAS_DJANGO_TASKS, "Requires django.tasks")
     def test_django_using_lib_backend(self) -> None:
-        from django.tasks import task_backends
+        from django.tasks import task, task_backends
 
         with override_settings(
             TASKS={
@@ -28,9 +32,17 @@ class DjangoCompatTestCase(SimpleTestCase):
         ):
             self.assertIsInstance(task_backends["default"], ImmediateBackend)
 
+            test_task_func_task = task(_test_task_func)
+            self.assertIsInstance(test_task_func_task, Task)
+
+            result = test_task_func_task.enqueue()
+            self.assertIsInstance(result, TaskResult)
+            self.assertIsNone(result.return_value)
+
     @skipUnless(HAS_DJANGO_TASKS, "Requires django.tasks")
     def test_lib_using_django_backend(self) -> None:
         from django.tasks.backends.immediate import ImmediateBackend
+        from django.tasks.base import Task, TaskResult
 
         with override_settings(
             TASKS={
@@ -40,6 +52,13 @@ class DjangoCompatTestCase(SimpleTestCase):
             }
         ):
             self.assertIsInstance(task_backends["default"], ImmediateBackend)
+
+            test_task_func_task = task(_test_task_func)
+            self.assertIsInstance(test_task_func_task, Task)
+
+            result = test_task_func_task.enqueue()
+            self.assertIsInstance(result, TaskResult)
+            self.assertIsNone(result.return_value)
 
     def test_compat_has_django_task(self) -> None:
         self.assertIn(Task, compat.TASK_CLASSES)
